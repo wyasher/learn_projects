067426eb7b8a17fb859f3872a0fd69405fd400a7
wyasher <wyasher@member.com>
2024-06-30 21:52:23 +0800

    csharp/wpf/ProductMonitor:定义用户控件和viewmodel

--- csharp/wpf/ProductMonitor/ProductMonitor/UserControls/MonitorUserControl.xaml.cs
+++ csharp/wpf/ProductMonitor/ProductMonitor/UserControls/MonitorUserControl.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,22 +12,17 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using ProductMonitor.ViewModel;
 
-namespace ProductMonitor
+namespace ProductMonitor.UserControls
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// MonitorUserControl.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MonitorUserControl : UserControl
     {
-        public MainWindow()
+        public MonitorUserControl()
         {
             InitializeComponent();
-            MainWindowViewModel mainWindowViewModel = new MainWindowViewModel();
-            DataContext = mainWindowViewModel;
         }
-      
     }
-  
 }
---
+++ csharp/wpf/ProductMonitor/ProductMonitor/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using ProductMonitor.UserControls;
+
+namespace ProductMonitor.ViewModel
+{
+
+    internal class MainWindowViewModel : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// 监控用户控件
+        /// </summary>
+        private UserControl? _monitorUserControl;
+
+        public UserControl MonitorUserControl
+        {
+            set
+            {
+                _monitorUserControl = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("MonitorUserControl"));
+            }
+            get
+            {
+                _monitorUserControl ??= new MonitorUserControl();
+                return _monitorUserControl;
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
