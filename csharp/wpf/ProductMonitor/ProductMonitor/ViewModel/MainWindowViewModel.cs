0ed137182b8850c526b8b2f0f7cb6ac1cf54dd65
wyasher <wyasher@member.com>
2024-07-02 22:04:30 +0800

    csharp/wpf/ProductMonitor:开发头部技术数据

--- csharp/wpf/ProductMonitor/ProductMonitor/ViewModel/MainWindowViewModel.cs
+++ csharp/wpf/ProductMonitor/ProductMonitor/ViewModel/MainWindowViewModel.cs
@@ -33,5 +33,64 @@ namespace ProductMonitor.ViewModel
         }
 
         public event PropertyChangedEventHandler? PropertyChanged;
+
+        public string TimeStr => DateTime.Now.ToString("HH:mm");
+        public string DateStr => DateTime.Now.ToString("yyyy-MM-dd");
+
+        private string[] weekDays = {"星期一","星期二","星期三","星期四","星期五","星期六","星期天" };
+
+        public string WeekStr
+        {
+            get
+            {
+                int index = (int)DateTime.Now.DayOfWeek;
+                return weekDays[index];
+            }
+        }
+
+        private string _machineCount = "0298";
+
+        public string MachineCount
+        {
+            set
+            {
+                _machineCount = value;
+               PropertyChanged?.Invoke(this,new PropertyChangedEventArgs("MachineCount"));
+            }
+            get
+            {
+                return _machineCount;
+            }
+        }
+
+        private string _productCount = "1643";
+
+        public string ProductCount
+        {
+            set
+            {
+                _productCount = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("ProductCount"));
+            }
+            get
+            {
+                return _productCount;
+            }
+        }
+
+        private string _badCount = "1633";
+
+        public string BadCount
+        {
+            set
+            {
+                _badCount = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("BadCount"));
+            }
+            get
+            {
+                return _badCount;
+            }
+        }
     }
 }
